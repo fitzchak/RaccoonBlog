e3b1b946f0174ff053d00d1b0f02ce45dd9ddf2f
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-04-14 15:26:27 +0300

    Extract handling of emails to a separate command

--- src/RavenDbBlog/Commands/AddCommentCommand.cs
+++ src/RavenDbBlog/Commands/AddCommentCommand.cs
@@ -1,15 +1,9 @@
 ﻿using System;
 using System.Configuration;
-using System.Linq;
-using System.Net.Mail;
-using System.Text;
 using System.Web.Mvc;
 using Joel.Net;
-using Newtonsoft.Json;
-using RavenDbBlog.Common;
+using Raven.Client;
 using RavenDbBlog.Core.Models;
-using RavenDbBlog.Domain;
-using RavenDbBlog.Infrastructure;
 using RavenDbBlog.ViewModels;
 using RazorEngine;
 
@@ -17,32 +11,29 @@ namespace RavenDbBlog.Commands
 {
     public class AddCommentCommand : ICommand
     {
-        private readonly CommentInput _commentInput;
-        private readonly int _postId;
+        public CommentInput CommentInput { get; set; }
+        public int PostId { get; set; }
 
-        public AddCommentCommand(CommentInput commentInput, int postId)
-        {
-            _commentInput = commentInput;
-            _postId = postId;
-        }
+        public IDocumentSession Session { get; set; }
 
         public void Execute()
         {
-            var session = RavenActionFilterAttribute.DocumentStore.OpenSession();
-            var postAndComments = new PostReader(session).GetPostAndComments(_postId);
-            var post = postAndComments.Item1;
+            Session.Load<object>("posts/" + PostId, "posts/" + PostId + "/comments");
+
+            var post = Session.Load<Post>("posts/" + PostId);
+            var comments = Session.Load<CommentsCollection>("posts/" + PostId + "/comments");
+
             post.CommentsCount++;
 
-            var comments = postAndComments.Item2;
             var comment = new CommentsCollection.Comment
             {
-                Author = _commentInput.Name,
-                Body = _commentInput.Body,
+                Author = CommentInput.Name,
+                Body = CommentInput.Body,
                 CreatedAt = DateTimeOffset.Now,  // TODO: Time zone of the client.
-                Email = _commentInput.Email,
-                Important = false,
-                Url = _commentInput.Url,
-                IsSpam = CheckForSpam(_commentInput),
+                Email = CommentInput.Email,
+                Important = false, //TODO: How to figure this out?
+                Url = CommentInput.Url,
+                IsSpam = CheckForSpam(),
             };
 
             if (comment.IsSpam)
@@ -54,55 +45,42 @@ namespace RavenDbBlog.Commands
                 comments.Comments.Add(comment);
             }
 
-            session.SaveChanges();
-
-            SendEmail(comment);
-        }
-
-        private void SendEmail(CommentsCollection.Comment comment)
-        {
-            var message = new MailMessage();
-
-            var commentsMederatorEmails = ConfigurationSettings.AppSettings["CommentsMederatorEmails"];
-            commentsMederatorEmails
-                .Split(';')
-                .Select(x => new MailAddress(x.Trim()))
-                .ForEach(email => message.To.Add(email));
-
-            var blogName = ConfigurationSettings.AppSettings["blogName"];
-            message.Subject = string.Format("A new comment on {0} blog", blogName);
-
             var vm = new NewCommentEmailViewModel
-                         {
-                             Author = comment.Author,
-                             Body = MvcHtmlString.Create(comment.Body),
-                             CreatedAt = comment.CreatedAt,
-                             Email = comment.Email,
-                             Url = comment.Url,
-                         };
-            message.Body = Razor.Run(vm, "NewComment");
+            {
+                Author = comment.Author,
+                Body = MvcHtmlString.Create(comment.Body),
+                CreatedAt = comment.CreatedAt,
+                Email = comment.Email,
+                Url = comment.Url,
+            };
 
-            message.IsBodyHtml = true;
-            new SendEmailCommand(message).Execute();
+            var emailContents = Razor.Run(vm, "NewComment");
+
+            CommandExcucator.ExcuteLater(new SendEmailCommand()
+                                             {
+                                                 Subject = "Comment: " + post.Title,
+                                                 Contents = emailContents
+                                             });
         }
 
-        public bool CheckForSpam(CommentInput comment)
+
+        private bool CheckForSpam()
         {
             //Create a new instance of the Akismet API and verify your key is valid.
             string blog = "http://" + ConfigurationSettings.AppSettings["MainUrl"];
-            var api = new Akismet(ConfigurationSettings.AppSettings["AkismetKey"], blog, _commentInput.Request.UserAgent);
+            var api = new Akismet(ConfigurationSettings.AppSettings["AkismetKey"], blog, CommentInput.Request.UserAgent);
             if (!api.VerifyKey()) throw new Exception("Akismet API key invalid.");
 
             var akismetComment = new AkismetComment
             {
                 Blog = blog,
-                UserIp = _commentInput.Request.UserHostAddress,
-                UserAgent = _commentInput.Request.UserAgent,
-                CommentContent = comment.Body,
+                UserIp = CommentInput.Request.UserHostAddress,
+                UserAgent = CommentInput.Request.UserAgent,
+                CommentContent = CommentInput.Body,
                 CommentType = "comment",
-                CommentAuthor = comment.Name,
-                CommentAuthorEmail = comment.Email,
-                CommentAuthorUrl = comment.Url,
+                CommentAuthor = CommentInput.Name,
+                CommentAuthorEmail = CommentInput.Email,
+                CommentAuthorUrl = CommentInput.Url,
             };
 
             //Check if Akismet thinks this comment is spam. Returns TRUE if spam.
