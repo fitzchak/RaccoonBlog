0958804d140e510783f1a2adc21e2b1149477120
Ayende Rahien <ayende@ayende.com>
2011-04-21 14:22:56 +0300

    Refactoring routing configuration

--- src/RavenDbBlog/RouteConfigurator.cs
+++ src/RavenDbBlog/RouteConfigurator.cs
@@ -1,79 +1,32 @@
+using System;
 using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RavenDbBlog
 {
-    public static class RouteConfigurator
+    public class RouteConfigurator
     {
         private const string MatchPositiveInteger = @"\d{1,10}";
 
-        public static void Configure(RouteCollection routes)
+    	private readonly RouteCollection routes;
+
+    	public RouteConfigurator(RouteCollection routes)
+    	{
+    		this.routes = routes;
+    	}
+
+
+    	public void Configure()
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("CommentOnPost",
-                "{id}/comment",
-                new { controller = "Post", action = "Comment" },
-                new { id = MatchPositiveInteger }
-                );
+    		ConfigureComments();
 
-            routes.MapRoute("RssFeed",
-              "rss",
-              new { controller = "Syndication", action = "Rss" }
-              );
+        	ConfigureRss();
 
-            routes.MapRoute("RssFeedByTag",
-              "rss/{name}",
-              new { controller = "Syndication", action = "Tag" }
-              );
+    		ConfigurePosts();
 
-
-            routes.MapRoute("RsdFeed",
-              "rsd",
-              new { controller = "Syndication", action = "Rsd" }
-              );
-
-
-            routes.MapRoute("PostById",
-                "{id}/{slug}",
-                new { controller = "Post", action = "Item", slug = UrlParameter.Optional },
-                new { id = MatchPositiveInteger }
-                );
-
-            routes.MapRoute("PostController",
-                "{controller}/{action}",
-                new { controller = "Post", action = "List" },
-                new { controller = "Post", action = "TagsList|ArchivesList" }
-                );
-
-            routes.MapRoute("PostsByTag",
-                "tags/{name}",
-                new { controller = "Post", action = "Tag" }
-                );
-
-            routes.MapRoute("RedirectLegacyPostUrl",
-                "archive/{year}/{month}/{day}/{slug}.aspx",
-                new { controller = "Post", action = "RedirectItem" },
-                new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
-                );
-
-            routes.MapRoute("PostsByYearMonthDay",
-                "archive/{year}/{month}/{day}",
-                new { controller = "Post", action = "ArchiveYearMonthDay" },
-                new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
-                );
-
-            routes.MapRoute("PostsByYearMonth",
-                "archive/{year}/{month}",
-                new { controller = "Post", action = "ArchiveYearMonth" },
-                new { Year = MatchPositiveInteger, Month = MatchPositiveInteger }
-                );
-
-            routes.MapRoute("PostsByYear",
-                "archive/{year}",
-                new { controller = "Post", action = "ArchiveYear" },
-                new { Year = MatchPositiveInteger }
-                );
+    		ConfigureArchive();
 
             routes.MapRoute("AllPosts",
                 "",
@@ -86,22 +39,104 @@ namespace RavenDbBlog
                new { action = "Login|LogOut|CurrentUser" }
                );
 
-            routes.MapRoute("UserAdminController",
-               "admin/users/{id}/{action}",
-               new { controller = "UserAdmin" },
-               new { action = "Edit|Details|Delete", id = MatchPositiveInteger }
-               );
+    		ConfigureAdmin();
 
-            routes.MapRoute("UserAdminController",
-               "admin/users/{action}",
-               new { controller = "UserAdmin" },
-               new { action = "Update|List" }
-               );
-
-            routes.MapRoute("Default",
+           routes.MapRoute("Default",
                 "",
                 new { controller = "Post", action = "List" }
                 );
         }
+
+    	private void ConfigureAdmin()
+    	{
+			routes.MapRoute("UserAdminController",
+			  "admin/users/{id}/{action}",
+			  new { controller = "UserAdmin" },
+			  new { action = "Edit|Details|Delete", id = MatchPositiveInteger }
+			  );
+
+			routes.MapRoute("UserAdminController",
+			   "admin/users/{action}",
+			   new { controller = "UserAdmin" },
+			   new { action = "Update|List" }
+			   );
+    	}
+
+    	private void ConfigureArchive()
+    	{
+
+			routes.MapRoute("RedirectLegacyPostUrl",
+				"archive/{year}/{month}/{day}/{slug}.aspx",
+				new { controller = "Post", action = "RedirectItem" },
+				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
+				);
+
+			routes.MapRoute("PostsByYearMonthDay",
+				"archive/{year}/{month}/{day}",
+				new { controller = "Post", action = "ArchiveYearMonthDay" },
+				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
+				);
+
+			routes.MapRoute("PostsByYearMonth",
+				"archive/{year}/{month}",
+				new { controller = "Post", action = "ArchiveYearMonth" },
+				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger }
+				);
+
+			routes.MapRoute("PostsByYear",
+				"archive/{year}",
+				new { controller = "Post", action = "ArchiveYear" },
+				new { Year = MatchPositiveInteger }
+				);
+    	}
+
+    	private void ConfigurePosts()
+    	{
+			routes.MapRoute("PostById",
+				"{id}/{slug}",
+				new { controller = "Post", action = "Item", slug = UrlParameter.Optional },
+				new { id = MatchPositiveInteger }
+				);
+
+			routes.MapRoute("PostController",
+				"{controller}/{action}",
+				new { controller = "Post", action = "List" },
+				new { controller = "Post", action = "TagsList|ArchivesList" }
+				);
+
+			routes.MapRoute("PostsByTag",
+				"tags/{name}",
+				new { controller = "Post", action = "Tag" }
+				);
+    	}
+
+    	private void ConfigureComments()
+    	{
+			routes.MapRoute("CommentOnPost",
+			 "{id}/comment",
+			 new { controller = "Post", action = "Comment" },
+			 new { id = MatchPositiveInteger }
+			 );
+    	}
+
+    	private void ConfigureRss()
+    	{
+
+			routes.MapRoute("RssFeed",
+			  "rss",
+			  new { controller = "Syndication", action = "Rss" }
+			  );
+
+			routes.MapRoute("RssFeedByTag",
+			  "rss/{name}",
+			  new { controller = "Syndication", action = "Tag" }
+			  );
+
+
+			routes.MapRoute("RsdFeed",
+			  "rsd",
+			  new { controller = "Syndication", action = "Rsd" }
+			  );
+    	}
     }
 }
