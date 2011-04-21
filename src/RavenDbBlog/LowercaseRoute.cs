ed55a9704755a8be4c95ca13824137c4c2789042
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-04-21 17:27:16 +0300

    Add LowerCase route class

---
+++ src/RavenDbBlog/LowercaseRoute.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RavenDbBlog
+{
+    public class LowercaseRoute : Route
+    {
+        public LowercaseRoute(string url, IRouteHandler routeHandler)
+            : base(url, routeHandler) { }
+        public LowercaseRoute(string url, RouteValueDictionary defaults, IRouteHandler routeHandler)
+            : base(url, defaults, routeHandler) { }
+        public LowercaseRoute(string url, RouteValueDictionary defaults, RouteValueDictionary constraints, IRouteHandler routeHandler)
+            : base(url, defaults, constraints, routeHandler) { }
+        public LowercaseRoute(string url, RouteValueDictionary defaults, RouteValueDictionary constraints, RouteValueDictionary dataTokens, IRouteHandler routeHandler) : base(url, defaults, constraints, dataTokens, routeHandler) { }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            VirtualPathData path = base.GetVirtualPath(requestContext, values);
+
+            if (path != null)
+                path.VirtualPath = path.VirtualPath.ToLowerInvariant();
+
+            return path;
+        }
+    }
+
+    public static class RouteCollectionExtension
+    {
+        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults)
+        {
+            return routes.MapRouteLowerCase(name, url, defaults, null);
+        }
+
+        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults, string constraints)
+        {
+            Route route = new LowercaseRoute(url, new MvcRouteHandler())
+            {
+                Defaults = new RouteValueDictionary(defaults),
+                Constraints = new RouteValueDictionary(constraints)
+            };
+
+            routes.Add(name, route);
+
+            return route;
+        }
+    }
+}
