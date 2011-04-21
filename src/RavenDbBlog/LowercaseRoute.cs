b429ee6d4f77731ff9523c7d9407364072ab74a3
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-04-21 17:33:13 +0300

    Use lower case routes

--- src/RavenDbBlog/LowercaseRoute.cs
+++ src/RavenDbBlog/LowercaseRoute.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace RavenDbBlog
@@ -31,12 +32,22 @@ namespace RavenDbBlog
             return routes.MapRouteLowerCase(name, url, defaults, null);
         }
 
-        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults, string constraints)
+        public static Route MapRouteLowerCase(this RouteCollection routes, string name, string url, object defaults, object constraints)
         {
+            if (routes == null)
+            {
+                throw new ArgumentNullException("routes");
+            }
+            if (url == null)
+            {
+                throw new ArgumentNullException("url");
+            }
+
             Route route = new LowercaseRoute(url, new MvcRouteHandler())
             {
                 Defaults = new RouteValueDictionary(defaults),
-                Constraints = new RouteValueDictionary(constraints)
+                Constraints = new RouteValueDictionary(constraints),
+                DataTokens = new RouteValueDictionary(),
             };
 
             routes.Add(name, route);
--- src/RavenDbBlog/RouteConfigurator.cs
+++ src/RavenDbBlog/RouteConfigurator.cs
@@ -27,7 +27,7 @@ namespace RavenDbBlog
 
     		ConfigureArchive();
 
-            routes.MapRoute("LoginController",
+            routes.MapRouteLowerCase("LoginController",
                "users/{action}",
                new { controller = "Login" },
                new { action = "Login|LogOut|CurrentUser" }
@@ -35,12 +35,12 @@ namespace RavenDbBlog
 
     		ConfigureAdmin();
 
-            routes.MapRoute("AllPosts",
+            routes.MapRouteLowerCase("AllPosts",
                 "",
                 new { controller = "Post", action = "List" }
                 );
 
-            routes.MapRoute("Default",
+            routes.MapRouteLowerCase("Default",
                 "",
                 new { controller = "Post", action = "List" }
                 );
@@ -48,19 +48,19 @@ namespace RavenDbBlog
 
     	private void ConfigureAdmin()
     	{
-			routes.MapRoute("UserAdminController-ActionWithId",
+			routes.MapRouteLowerCase("UserAdminController-ActionWithId",
 			  "admin/users/{id}/{action}",
 			  new { controller = "UserAdmin" },
 			  new { action = "Edit|Details|Delete", id = MatchPositiveInteger }
 			  );
 
-			routes.MapRoute("UserAdminController-Action",
+			routes.MapRouteLowerCase("UserAdminController-Action",
 			   "admin/users/{action}",
 			   new { controller = "UserAdmin" },
 			   new { action = "Add|Update" }
 			   );
 
-            routes.MapRoute("UserAdminController-UsersList",
+            routes.MapRouteLowerCase("UserAdminController-UsersList",
                "admin/users",
                new { controller = "UserAdmin", action = "List" }
                );
@@ -69,25 +69,25 @@ namespace RavenDbBlog
     	private void ConfigureArchive()
     	{
 
-			routes.MapRoute("RedirectLegacyPostUrl",
+			routes.MapRouteLowerCase("RedirectLegacyPostUrl",
 				"archive/{year}/{month}/{day}/{slug}.aspx",
 				new { controller = "Post", action = "RedirectItem" },
 				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
 				);
 
-			routes.MapRoute("PostsByYearMonthDay",
+			routes.MapRouteLowerCase("PostsByYearMonthDay",
 				"archive/{year}/{month}/{day}",
 				new { controller = "Post", action = "ArchiveYearMonthDay" },
 				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger, Day = MatchPositiveInteger }
 				);
 
-			routes.MapRoute("PostsByYearMonth",
+			routes.MapRouteLowerCase("PostsByYearMonth",
 				"archive/{year}/{month}",
 				new { controller = "Post", action = "ArchiveYearMonth" },
 				new { Year = MatchPositiveInteger, Month = MatchPositiveInteger }
 				);
 
-			routes.MapRoute("PostsByYear",
+			routes.MapRouteLowerCase("PostsByYear",
 				"archive/{year}",
 				new { controller = "Post", action = "ArchiveYear" },
 				new { Year = MatchPositiveInteger }
@@ -96,19 +96,19 @@ namespace RavenDbBlog
 
     	private void ConfigurePosts()
     	{
-			routes.MapRoute("PostById",
+			routes.MapRouteLowerCase("PostById",
 				"{id}/{slug}",
 				new { controller = "Post", action = "Item", slug = UrlParameter.Optional },
 				new { id = MatchPositiveInteger }
 				);
 
-			routes.MapRoute("PostController",
+			routes.MapRouteLowerCase("PostController",
 				"{controller}/{action}",
 				new { controller = "Post", action = "List" },
 				new { controller = "Post", action = "TagsList|ArchivesList" }
 				);
 
-			routes.MapRoute("PostsByTag",
+			routes.MapRouteLowerCase("PostsByTag",
 				"tags/{name}",
 				new { controller = "Post", action = "Tag" }
 				);
@@ -116,7 +116,7 @@ namespace RavenDbBlog
 
     	private void ConfigureComments()
     	{
-			routes.MapRoute("CommentOnPost",
+			routes.MapRouteLowerCase("CommentOnPost",
 			 "{id}/comment",
 			 new { controller = "Post", action = "Comment" },
 			 new { id = MatchPositiveInteger }
@@ -126,18 +126,18 @@ namespace RavenDbBlog
     	private void ConfigureRss()
     	{
 
-			routes.MapRoute("RssFeed",
+			routes.MapRouteLowerCase("RssFeed",
 			  "rss",
 			  new { controller = "Syndication", action = "Rss" }
 			  );
 
-			routes.MapRoute("RssFeedByTag",
+			routes.MapRouteLowerCase("RssFeedByTag",
 			  "rss/{name}",
 			  new { controller = "Syndication", action = "Tag" }
 			  );
 
 
-			routes.MapRoute("RsdFeed",
+			routes.MapRouteLowerCase("RsdFeed",
 			  "rsd",
 			  new { controller = "Syndication", action = "Rsd" }
 			  );
