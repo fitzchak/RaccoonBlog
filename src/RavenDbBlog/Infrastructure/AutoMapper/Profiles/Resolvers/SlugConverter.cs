069156fb9a46ff92827189a99c9a7e30adbbb33e
Fitzchak Yitzchaki <fitzchak@ayende.com>
2011-04-23 21:25:47 +0300

    SlugConverter should not be a AutoMapper converter

--- src/RavenDbBlog/Infrastructure/AutoMapper/Profiles/Resolvers/SlugConverter.cs
+++ src/RavenDbBlog/Infrastructure/AutoMapper/Profiles/Resolvers/SlugConverter.cs
@@ -2,13 +2,8 @@
 
 namespace RavenDbBlog.Infrastructure.AutoMapper.Profiles.Resolvers
 {
-    public class SlugConverter : TypeConverter<string, string>
+    public class SlugConverter
     {
-        protected override string ConvertCore(string source)
-        {
-            return TitleToSlag(source);
-        }
-
         public static string TitleToSlag(string title)
         {
             return title;
