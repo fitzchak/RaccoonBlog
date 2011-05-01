6e82bf95a66acf9511206df5fc364f00a811f1fd
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-01 16:18:35 +0300

    Converts HTML comment to markdown

--- src/Import/Program.cs
+++ src/Import/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Xsl;
 using Raven.Client.Document;
 using RavenDbBlog.Core.Models;
 using RavenDbBlog.Infrastructure.EntityFramework;
@@ -12,6 +18,9 @@ namespace RavenDbBlog.Import
     {
         static void Main(string[] args)
         {
+            //GetAllHtmlFromComments();
+            //return;
+
             using (var e = new SubtextEntities())
             {
             	Console.WriteLine("Starting...");
@@ -22,7 +31,7 @@ namespace RavenDbBlog.Import
                     .Include("Links")
                     .Include("Links.Categories")
                     .ToList()
-					.OrderBy(x=>x.DateSyndicated);
+                    .OrderBy(x => x.DateSyndicated);
 
                 Console.WriteLine("Loading data took {0:#,#} ms", sp.ElapsedMilliseconds);
 
@@ -33,28 +42,28 @@ namespace RavenDbBlog.Import
                                            Url = "http://localhost:8080",
                                        }.Initialize())
                 {
-					using(var s = store.OpenSession())
-					{
-					    var users = new[]
+                    using (var s = store.OpenSession())
+                    {
+                        var users = new[]
 					        {
 					            new {Email = "ayende@ayende.com", FullName = "Ayende Rahien"},
 					            new {Email = "fitzchak@ayende.com", FullName = "Fitzchak Yitzchaki"},
 					        };
 
-					    for (int i = 0; i < users.Length; i++)
-					    {
-					        var user = new User
-					            {
+                        for (int i = 0; i < users.Length; i++)
+                        {
+                            var user = new User
+                                {
                                     Id = "users/" + (i + 1),
                                     Email = users[i].Email,
                                     FullName = users[i].FullName,
                                     Enabled = true,
-					            };
+                                };
                             user.SetPassword("123456");
                             s.Store(user);
-					    }
-						s.SaveChanges();
-					}
+                        }
+                        s.SaveChanges();
+                    }
 
                     foreach (var post in theEntireDatabaseOhMygod)
                     {
@@ -67,7 +76,7 @@ namespace RavenDbBlog.Import
                             CommentsCount = post.FeedBackCount,
                             LegacySlug = post.EntryName,
                             Title = post.Title,
-                            Tags = post.Links.Select(x=>x.Categories.Title)
+                            Tags = post.Links.Select(x => x.Categories.Title)
                                 .Where(x => x != "Uncategorized")
                                 .ToArray()
                         };
@@ -81,7 +90,7 @@ namespace RavenDbBlog.Import
                                     {
                                         Id = commentsCollection.GenerateNewCommentId(),
                                         Author = comment.Author,
-                                        Body = comment.Body,
+                                        //Body = ConvertCommentToMarkdown(comment.Body),
                                         CreatedAt = comment.DateCreated,
                                         Email = comment.Email,
                                         Important = comment.IsBlogAuthor ?? false,
@@ -97,7 +106,7 @@ namespace RavenDbBlog.Import
                                     {
                                         Id = commentsCollection.GenerateNewCommentId(),
                                         Author = comment.Author,
-                                        Body = comment.Body,
+                                        //Body = ConvertCommentToMarkdown(comment.Body),
                                         CreatedAt = comment.DateCreated,
                                         Email = comment.Email,
                                         Important = comment.IsBlogAuthor ?? false,
@@ -123,5 +132,99 @@ namespace RavenDbBlog.Import
             }
 
         }
+
+        private static void GetAllHtmlFromComments()
+        {
+            using (var e = new SubtextEntities())
+            {
+                int c = 0;
+                foreach (var comment in e.Comments.ToArray())
+                {
+                    if (IsHtmlComment(comment.Body))
+                    {
+                        //Console.WriteLine("Id: " + comment.Id + " Comment: " + comment.Body + Environment.NewLine + Environment.NewLine);
+                        c++;
+                        //if (c > 10)
+                        //{
+                        //    Console.WriteLine("Break: " + c);
+                        //    break;
+                        //}
+                    }
+                }
+            }
+        }
+
+        static HashSet<string> seen = new HashSet<string>();
+
+        private static bool IsHtmlComment(string body)
+        {
+            var match = Regex.Match(ConvertCommentToMarkdown(body), @"</[^ac]\w+>"); // <[^ =T>.</acu]
+            if (match.Success)
+            {
+                if (seen.Add(match.Value))
+                    Console.WriteLine("Match: " + match.Value);
+                return true;
+            }
+            return false;
+        }
+
+        private static string ConvertCommentToMarkdown(string body)
+        {
+            return HtmlToMarkdown(body);
+
+            body = body.Replace("<br />", "  " + Environment.NewLine);
+
+            body = body.Replace("<strong>", "**");
+            body = body.Replace("</strong>", "**");
+            body = body.Replace("<b>", "**");
+            body = body.Replace("</b>", "**");
+
+            body = body.Replace("<i>", "*");
+            body = body.Replace("</i>", "*");
+            body = body.Replace("<em>", "*");
+            body = body.Replace("</em>", "*");
+
+            body = body.Replace("</pre>", "*");
+            body = body.Replace("</quote>", "*");
+            body = body.Replace("</h1>", "*");
+            body = body.Replace("</a>", "*");
+            body = body.Replace("</code>", "*");
+
+            return body;
+        }
+
+        public static string HtmlToMarkdown(string html)
+        {
+            var xslt = new XslCompiledTransform();
+            xslt.Load("markdown.xsl");
+
+            // Execute the transform and output the results to a file.
+            var buffer = new StringBuilder();
+            var writer1 = new StringWriter(buffer);
+            var writer = new XmlTextWriter(writer1);
+
+            var doc = FromHtml(new StringReader(html));
+            xslt.Transform(new XmlNodeReader(doc), writer);
+
+            return buffer.ToString();
+        }
+
+        private static XmlDocument FromHtml(TextReader reader)
+        {
+            // setup SGMLReader
+            Sgml.SgmlReader sgmlReader = new Sgml.SgmlReader();
+            sgmlReader.DocType = "HTML";
+            sgmlReader.WhitespaceHandling = WhitespaceHandling.All;
+            sgmlReader.CaseFolding = Sgml.CaseFolding.ToLower;
+            sgmlReader.InputStream = reader;
+
+            // create document
+            var doc = new XmlDocument();
+            doc.PreserveWhitespace = true;
+            doc.XmlResolver = null;
+            doc.Load(sgmlReader);
+            return doc;
+        }
+
     }
 }
