dadca2feaa450facd4afe292abb0477d434790b0
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-02 15:39:02 +0300

    Add RequiredIfAttribute

---
+++ src/RavenDbBlog/ViewModels/SectionInput.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using RavenDbBlog.Helpers.Validation;
+
+namespace RavenDbBlog.ViewModels
+{
+    public class SectionInput
+    {
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [AllowHtml]
+        [Display(Name = "Body")]
+        public string Body { get; set; }
+
+        [RequiredIf("Body", "")]
+        [Display(Name = "Controller Name")]
+        public string ControllerName { get; set; }
+
+        [RequiredIf("Body", "")]
+        [Display(Name = "Action Name")]
+        public string ActionName { get; set; }
+
+        public bool IsActionSection()
+        {
+            return string.IsNullOrEmpty(Body);
+        }
+    }
+}
