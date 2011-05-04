3ce09115150e8c04cb27b4dfda0ea361fdb4a509
Ayende Rahien <ayende@ayende.com>
2011-05-04 15:20:02 +0300

    Move ArchivesList and TagsList to the section controller

--- src/RaccoonBlog.Web/Controllers/SectionController.cs
+++ src/RaccoonBlog.Web/Controllers/SectionController.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using RaccoonBlog.Web.Infrastructure.AutoMapper;
+using RaccoonBlog.Web.Infrastructure.Indexes;
 using RaccoonBlog.Web.Models;
 using RaccoonBlog.Web.ViewModels;
+using Raven.Client.Linq;
 
 namespace RaccoonBlog.Web.Controllers
 {
@@ -17,5 +20,35 @@ namespace RaccoonBlog.Web.Controllers
 
             return View(sections.MapTo<SectionDetails>());
         }
+
+
+		[ChildActionOnly]
+		public ActionResult TagsList()
+		{
+			var mostRecentTag = new DateTimeOffset(DateTimeOffset.Now.Year - 2,
+												   DateTimeOffset.Now.Month,
+												   1, 0, 0, 0,
+												   DateTimeOffset.Now.Offset);
+
+			var tagCounts = Session.Query<TagCount, Tags_Count>()
+				.Where(x => x.Count > 20 && x.LastSeenAt > mostRecentTag)
+				.OrderBy(x => x.Name)
+				.As<TempTagCount>();
+			var tags = tagCounts
+				.ToList();
+
+			return View(tags.MapTo<TagsListViewModel>());
+		}
+
+		[ChildActionOnly]
+		public ActionResult ArchivesList()
+		{
+			var dates = Session.Query<PostCountByMonth, Posts_ByMonthPublished_Count>()
+				.OrderByDescending(x => x.Year)
+				.ThenByDescending(x => x.Month)
+				.ToList();
+
+			return View(dates);
+		}
     }
 }
