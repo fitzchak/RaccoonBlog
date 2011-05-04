57647899eb57473040843429a4a79f4bb75f5d0d
Ayende Rahien <ayende@ayende.com>
2011-05-04 14:28:48 +0300

    Moving ElmahResult to a separate file

--- src/RaccoonBlog.Web/Controllers/ElmahController.cs
+++ src/RaccoonBlog.Web/Controllers/ElmahController.cs
@@ -1,4 +1,3 @@
-using System.Web;
 using System.Web.Mvc;
 
 namespace RaccoonBlog.Web.Controllers
@@ -13,30 +12,6 @@ namespace RaccoonBlog.Web.Controllers
         public ActionResult Detail(string type)
         {
             return new ElmahResult(type);
-        }
-    }
-
-    public class ElmahResult : ActionResult
-    {
-        private string _resouceType;
-
-        public ElmahResult(string resouceType)
-        {
-            _resouceType = resouceType;
-        }
-
-        public override void ExecuteResult(ControllerContext context)
-        {
-            var factory = new Elmah.ErrorLogPageFactory();
-            if (!string.IsNullOrEmpty(_resouceType))
-            {
-                var pathInfo = "." + _resouceType;
-                HttpContext.Current.RewritePath("/admin/elmah", pathInfo, HttpContext.Current.Request.QueryString.ToString());
-            }
-
-            var handler = factory.GetHandler(HttpContext.Current, null, null, null);
-
-            handler.ProcessRequest(HttpContext.Current);
         }
     }
 }
---
+++ src/RaccoonBlog.Web/Helpers/Results/ElmahResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace RaccoonBlog.Web.Controllers
+{
+	public class ElmahResult : ActionResult
+	{
+		private string _resouceType;
+
+		public ElmahResult(string resouceType)
+		{
+			_resouceType = resouceType;
+		}
+
+		public override void ExecuteResult(ControllerContext context)
+		{
+			var factory = new Elmah.ErrorLogPageFactory();
+			if (!string.IsNullOrEmpty(_resouceType))
+			{
+				var pathInfo = "." + _resouceType;
+				HttpContext.Current.RewritePath("/admin/elmah", pathInfo, HttpContext.Current.Request.QueryString.ToString());
+			}
+
+			var handler = factory.GetHandler(HttpContext.Current, null, null, null);
+
+			handler.ProcessRequest(HttpContext.Current);
+		}
+	}
+}
