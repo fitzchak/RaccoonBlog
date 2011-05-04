8fd11845382be19543a8f74ab6a94e2371ff95d8
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-04 12:11:29 +0300

    Fix type in class name

--- src/RavenDbBlog/Helpers/RecaptchHelperExtensions.cs
+++ src/RavenDbBlog/Helpers/RecaptchHelperExtensions.cs
@@ -6,7 +6,7 @@ using Recaptcha;
 
 namespace RavenDbBlog.Helpers
 {
-    public static class RecaptchHelperExtantions
+    public static class RecaptchHelperExtensions
     {
         public static MvcHtmlString GenerateCaptcha(this HtmlHelper helper)
         {
