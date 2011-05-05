e1e293eae63fd03932dc534f2bc9480a2a72c35e
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-05 15:39:57 +0300

    Fix mappings

--- src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/EmailViewModelMapperProfile.cs
+++ src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/EmailViewModelMapperProfile.cs
@@ -9,7 +9,9 @@ namespace RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles
         protected override void Configure()
         {
             Mapper.CreateMap<PostComments.Comment, NewCommentEmailViewModel>()
+                .ForMember(x => x.PostId, o => o.Ignore())
                 .ForMember(x => x.PostTitle, o => o.Ignore())
+                .ForMember(x => x.BlogName, o => o.Ignore())
                 ;
         }
     }
