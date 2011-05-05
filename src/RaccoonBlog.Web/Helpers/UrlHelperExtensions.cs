aac6fb1f537f217f91856b24329bbbc95b13aaeb
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-05 16:37:24 +0300

    Add canonical tag for post details page. Add a way to have absolute links.

---
+++ src/RaccoonBlog.Web/Helpers/UrlHelperExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Web.Mvc;
+
+namespace RaccoonBlog.Web.Helpers
+{
+    public static class UrlHelperExtensions
+    {
+        public static string AbsoluteAction(this UrlHelper url, string action, object routeValues)
+        {
+            return AbsoluteAction(url, url.Action(action, routeValues));
+        }
+
+        public static string AbsoluteAction(this UrlHelper url, string action, string controller)
+        {
+            return AbsoluteAction(url, url.Action(action, controller));
+        }
+
+        private static string AbsoluteAction(UrlHelper url, string relativeUrl)
+        {
+            Uri requestUrl = url.RequestContext.HttpContext.Request.Url;
+            string absoluteAction = string.Format("{0}://{1}{2}",
+                                                  requestUrl.Scheme,
+                                                  requestUrl.Authority,
+                                                  relativeUrl);
+            return absoluteAction;
+        }
+    }
+}
