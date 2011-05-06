94b02609b07b9583a7cccbfacde8189a7648ae0b
Ayende Rahien <ayende@ayende.com>
2011-05-06 19:28:29 +0300

    Simplifying error handling. Output exception information in the error page

--- src/RaccoonBlog.Web/Helpers/Attributes/ElmahHandleErrorAttribute.cs
+++ src/RaccoonBlog.Web/Helpers/Attributes/ElmahHandleErrorAttribute.cs
@@ -9,46 +9,9 @@ namespace RaccoonBlog.Web.Helpers
     {
         public override void OnException(ExceptionContext context)
         {
-            View = "500";   // Look for 500 view in shared folder.
-            base.OnException(context);
-
-            var e = context.Exception;
-            if (!context.ExceptionHandled   // if unhandled, will be logged anyhow
-                || RaiseErrorSignal(e)      // prefer signaling, if possible
-                || IsFiltered(context))     // filtered?
-                return;
-
-            LogException(e);
-        }
-
-        private static bool RaiseErrorSignal(Exception e)
-        {
-            var context = HttpContext.Current;
-            if (context == null)
-                return false;
-            var signal = ErrorSignal.FromContext(context);
-            if (signal == null)
-                return false;
-            signal.Raise(e, context);
-            return true;
-        }
-
-        private static bool IsFiltered(ExceptionContext context)
-        {
-            var config = context.HttpContext.GetSection("elmah/errorFilter") as ErrorFilterConfiguration;
-
-            if (config == null)
-                return false;
-
-            var testContext = new ErrorFilterModule.AssertionHelperContext(context.Exception, HttpContext.Current);
-
-            return config.Assertion.Test(testContext);
-        }
-
-        private static void LogException(Exception e)
-        {
-            var context = HttpContext.Current;
-            ErrorLog.GetDefault(context).Log(new Error(e, context));
+        	View = "500";
+			ErrorLog.GetDefault(HttpContext.Current).Log(new Error(context.Exception, HttpContext.Current));
+			base.OnException(context);
         }
     }
 }
