49247ac549bcc25bba43069062d94b2146ab4a83
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-08 13:49:51 +0300

    Cache the css file generated by CssController

--- src/RaccoonBlog.Web/Controllers/CssController.cs
+++ src/RaccoonBlog.Web/Controllers/CssController.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using dotless.Core.configuration;
 
@@ -20,16 +21,17 @@ namespace RaccoonBlog.Web.Controllers
                  }
                  if (System.IO.File.Exists(normalizeFile))
                  {
+                     Response.AddFileDependency(normalizeFile);
                      builder.AppendLine(System.IO.File.ReadAllText(normalizeFile));
                  }
              }
 
-             // take care of etags!
+             Response.Cache.VaryByParams["files"] = true;
+             Response.Cache.SetLastModifiedFromFileDependencies();
+             Response.Cache.SetETagFromFileDependencies();
+             Response.Cache.SetCacheability(HttpCacheability.Public);
 
-             var css = dotless.Core.Less.Parse(builder.ToString(), new DotlessConfiguration
-                                                             {
-
-                                                             });
+             var css = dotless.Core.Less.Parse(builder.ToString(), new DotlessConfiguration());
 
              return Content(css, "text/css");
          }
