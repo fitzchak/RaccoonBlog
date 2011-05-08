70169941c219de47d3f4349605ea7500b26e6c57
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-08 09:44:55 +0300

    Add mappings

---
+++ src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/BlogConfigMapperProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using RaccoonBlog.Web.Models;
+using RaccoonBlog.Web.ViewModels;
+
+namespace RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles
+{
+    public class BlogConfigMapperProfile : Profile
+    {
+        protected override void Configure()
+        {
+            Mapper.CreateMap<BlogConfig, BlogConfigViewModel>()
+                ;
+        }
+    }
+}
