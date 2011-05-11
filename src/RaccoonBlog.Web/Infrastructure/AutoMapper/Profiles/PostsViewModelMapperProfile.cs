f87239a3e2cbfe4ea82342a1d38dfd0c62d16e5c
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-11 16:59:24 +0300

    Make AutoMapper tests to run

--- src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/PostsViewModelMapperProfile.cs
+++ src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/PostsViewModelMapperProfile.cs
@@ -13,13 +13,13 @@ namespace RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles
         {
             Mapper.CreateMap<Post, PostsViewModel.PostSummary>()
                 .ForMember(x => x.Id, o => o.MapFrom(m => RavenIdResolver.Resolve(m.Id)))
+                .ForMember(x => x.Author, o => o.Ignore())
                 .ForMember(x => x.Slug, o => o.MapFrom(m => SlugConverter.TitleToSlag(m.Title)))
                 .ForMember(x => x.PublishedAt, o => o.MapFrom(m => m.PublishAt))
                 .ForMember(x => x.Tags, o => o.MapFrom(m => m.Tags.Select(name => new TagDetails { Name = name })))
                 ;
 
 			Mapper.CreateMap<User, PostsViewModel.PostSummary.UserDetails>();
-    
         }
     }
 }
