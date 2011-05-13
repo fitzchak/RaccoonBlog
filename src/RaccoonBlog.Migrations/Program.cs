c69ad77f7e61e20188d8f2e176e52f74259e08a2
Ayende Rahien <ayende@ayende.com>
2011-05-13 09:55:58 +0300

    Adding migrations for the HTML encoding

---
+++ src/RaccoonBlog.Migrations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using RaccoonBlog.Web.Models;
+using Raven.Client.Document;
+
+namespace RaccoonBlog.Migrations
+{
+	class Program
+	{
+		static void Main()
+		{
+			using(var store = new DocumentStore{Url = "http://localhost:9191"}.Initialize())
+			{
+				int start = 0;
+				while (true)
+				{
+					using(var session = store.OpenSession())
+					{
+						var posts = session.Query<Post>()
+							.Skip(start)
+							.Take(128)
+							.ToList();
+
+						if (posts.Count == 0)
+							break;
+
+						foreach (var post in posts)
+						{
+							post.Title = HttpUtility.HtmlDecode(post.Title);
+						}
+
+						session.SaveChanges();
+					}
+
+					start += 128;
+				}
+			}
+		}
+	}
+}
