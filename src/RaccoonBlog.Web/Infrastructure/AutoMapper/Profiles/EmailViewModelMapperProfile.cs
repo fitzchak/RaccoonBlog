d082a18f1c3f35a5f087d1c514ce146678f7f980
Ayende Rahien <ayende@ayende.com>
2011-05-19 12:01:14 +0300

    Send the comment contents as HTML

--- src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/EmailViewModelMapperProfile.cs
+++ src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/EmailViewModelMapperProfile.cs
@@ -1,4 +1,5 @@
 using AutoMapper;
+using RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles.Resolvers;
 using RaccoonBlog.Web.Models;
 using RaccoonBlog.Web.ViewModels;
 
@@ -11,6 +12,7 @@ namespace RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles
             Mapper.CreateMap<PostComments.Comment, NewCommentEmailViewModel>()
                 .ForMember(x => x.PostId, o => o.Ignore())
                 .ForMember(x => x.PostTitle, o => o.Ignore())
+				.ForMember(x => x.Body, o => o.MapFrom(m => MarkdownResolver.Resolve(m.Body)))
                 .ForMember(x => x.BlogName, o => o.Ignore())
                 ;
         }
