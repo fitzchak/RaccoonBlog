e4765693b6f7a1e82a3e898f25d0e2debc29424f
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-31 10:35:23 +0300

    Refactor JS code out of the details view to a separate file

---
+++ src/RaccoonBlog.Web/Content/js/raccoon-blog.js
@@ -0,0 +1,81 @@
+
+// Common
+String.prototype.startsWith = function (str) {
+    return this.substr(0, str.length) === str;
+};
+String.prototype.isNullOrEmpty = function () {
+    return this == false || this === '';
+};
+
+(function (window, undefined) {
+    var Raccoon = {};
+    Raccoon.Util = {};
+    Raccoon.Util.Url = {};
+    Raccoon.Util.Url.toAbsolute = function (url) {
+        if (url.isNullOrEmpty()) return null;
+        if (url.startsWith("http://") || url.startsWith("https://")) return url;
+        return "http://" + url;
+    };
+
+    Raccoon.Views = {};
+    Raccoon.Views.Details = function() {
+        function insertComment() {
+            if ($('.livecomment').length > 0) return;
+
+            var now = new Date();
+            var comment = {
+                author: $('article#postComment input[name$="Name"]').val(),
+                emailHash: $.md5($('article#postComment input[name$="Email"]').val()),
+                url: $('article#postComment input[name$="Url"]').val(),
+                body: converter.makeHtml($('article#postComment textarea[name$="Body"]').val()),
+                createdAt: now.f("MM/dd/yyyy HH:mm")
+            };
+            $('#commentTemplate').tmpl(comment).appendTo('section.comments').show('medium');
+        };
+        
+        var $preview = null;
+        var converter = new Showdown.converter();
+        $('textarea[name$="Body"]').keydown(function () {
+            if ($preview == null) {
+                insertComment();
+                $preview = $('.livecomment .comment-body');
+            }
+            $preview.html(converter.makeHtml($(this).val()));
+        });
+        var $email = null;
+        $('input[name$="Email"]').keyup(function () {
+            if ($email == null) {
+                insertComment();
+                $email = $('.livecomment .avatar');
+            }
+            $email.attr('src', 'http://www.gravatar.com/avatar.php?gravatar_id=' + $.md5($(this).val()) + '&size=50&default=identicon');
+        });
+        var $name = null;
+        var name = null;
+        $('input[name$="Name"]').keyup(function () {
+            if ($name == null) {
+                insertComment();
+                $name = $('.livecomment address a');
+            }
+            name = $(this).val();
+            $name.text(name);
+        });
+        var $url = null;
+        $('input[name$="Url"]').keyup(function () {
+            if ($url == null) {
+                insertComment();
+                $url = $('.livecomment address a');
+            }
+            var url = Raccoon.Util.Url.toAbsolute($(this).val());
+            if (url) {
+                $url.attr('href', url);
+                $url.attr('title', url);
+            } else {
+                $url.removeAttr('href');
+                $url.attr('title', name);
+            }
+        });
+    };
+    
+    window.Raccoon = Raccoon;
+})(window);
