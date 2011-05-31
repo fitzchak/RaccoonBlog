d4b8d81bdfdc31b39036ad2ff5efb8e6b64ec995
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-05-31 13:00:08 +0300

    Refactoring - Use ICollection instead of array

--- src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/Resolvers/TagsResolver.cs
+++ src/RaccoonBlog.Web/Infrastructure/AutoMapper/Profiles/Resolvers/TagsResolver.cs
@@ -1,20 +1,21 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace RaccoonBlog.Web.Infrastructure.AutoMapper.Profiles.Resolvers
 {
-    public class TagsResolver
-    {
-        private const string TagsSeparator = "|";
+	public class TagsResolver
+	{
+		private const string TagsSeparator = "|";
 
-        public static string ResolveTags(string[] tags)
-        {	
-        	return string.Join(TagsSeparator, tags);
-        }
+		public static string ResolveTags(ICollection<String> tags)
+		{	
+			return string.Join(TagsSeparator, tags);
+		}
 
-        public static string[] ResolveTagsInput(string tags)
-        {
-            return tags.Split(new[] {TagsSeparator}, StringSplitOptions.RemoveEmptyEntries);
-        }
-    }
+		public static ICollection<String> ResolveTagsInput(string tags)
+		{
+			return tags.Split(new[] {TagsSeparator}, StringSplitOptions.RemoveEmptyEntries);
+		}
+	}
 }
