38ed1573d398c6c0028717adecd6b66f7c16cffa
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-06 23:55:12 +0300

    Add comment

--- src/RaccoonBlog.Web/ViewModels/PostReference.cs
+++ src/RaccoonBlog.Web/ViewModels/PostReference.cs
@@ -3,27 +3,28 @@ using RaccoonBlog.Web.Infrastructure.Common;
 
 namespace RaccoonBlog.Web.ViewModels
 {
-    public class PostReference
-    {
-        public string Id { get; set; }
-        public string Title { get; set; }
+	public class PostReference
+	{
+		// if Id is: posts/1024, than domainId will be: 1024
+		public string Id { get; set; }
+		public string Title { get; set; }
 
-        private int _domainId;
-        public int DomainId
-        {
-            get
-            {
-                if (_domainId == 0)
-                    _domainId = RavenIdResolver.Resolve(Id);
-                return _domainId;
-            }
-        }
+		private int domainId;
+		public int DomainId
+		{
+			get
+			{
+				if (domainId == 0)
+					domainId = RavenIdResolver.Resolve(Id);
+				return domainId;
+			}
+		}
 
-        private string _slug;
-        public string Slug
-        {
-            get { return _slug ?? (_slug = SlugConverter.TitleToSlug(Title)); }
-            set { _slug = value; }
-        }
-    }
+		private string slug;
+		public string Slug
+		{
+			get { return slug ?? (slug = SlugConverter.TitleToSlug(Title)); }
+			set { slug = value; }
+		}
+	}
 }
