90fd6583856ea65d86344d0c3096a3243a30dafe
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-12 13:56:40 +0300

    minor

--- src/RaccoonBlog.Web/Controllers/SearchController.cs
+++ src/RaccoonBlog.Web/Controllers/SearchController.cs
@@ -3,16 +3,16 @@ using System.Web.Mvc;
 namespace RaccoonBlog.Web.Controllers
 {
 	public class SearchController : AbstractController
-    {
-        public ActionResult SearchResult(string q)
-        {
+	{
+		public ActionResult SearchResult(string q)
+		{
 			object model = q;
 			return View(model);
-        } 
+		} 
 		
 		public ActionResult GoogleCse()
-        {
-        	return View();
-        }
-    }
+		{
+			return View();
+		}
+	}
 }
