26a41e914caef2fc999c94a9b42564b527dc4d5c
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-12 14:26:17 +0300

    Add built-in support for Google Analytics

--- src/RaccoonBlog.Web/Models/BlogConfig.cs
+++ src/RaccoonBlog.Web/Models/BlogConfig.cs
@@ -11,6 +11,7 @@ namespace RaccoonBlog.Web.Models
 		public string Copyright { get; set; }
 		public string AkismetKey { get; set; }
 		public string GoogleApiKey { get; set; }
+		public string GoogleAnalyticsKey { get; set; }
 
 		public string MetaDescription { get; set; }
 		public string MetaKeywords { get; set; }
