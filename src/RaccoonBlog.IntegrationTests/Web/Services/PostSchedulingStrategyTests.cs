80e11680d27dd6b7a8b2d11ab2a96e89028c7713
Fitzchak Yitzchak <fitzchak@ayende.com>
2011-06-16 16:00:11 +0300

    Add PostSchedulingStrategyTests and use Raven.Client.Embadded as a great way to test things against fake database.

---
+++ src/RaccoonBlog.IntegrationTests/Web/Services/PostSchedulingStrategyTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using RaccoonBlog.Web.Infrastructure.Common;
+using RaccoonBlog.Web.Models;
+using RaccoonBlog.Web.Services;
+using Raven.Client;
+using Raven.Client.Embedded;
+using Xunit;
+
+namespace RaccoonBlog.IntegrationTests.Web.Services
+{
+	public class PostSchedulingStrategyTests
+	{
+		private DateTimeOffset now;
+		public IDocumentSession Session { get; set; }
+
+		public PostSchedulingStrategyTests()
+		{
+			now = DateTimeOffset.Now;
+
+			var documentStore = new EmbeddableDocumentStore { RunInMemory = true }.Initialize();
+			Session = documentStore.OpenSession();
+		}
+
+		~PostSchedulingStrategyTests()
+		{
+			Session.Dispose();
+		}
+
+		[Fact]
+		public void WhenPostingNewPostWithoutPublishDateSpecified_AndTheLastPostPublishDateIsAFewDaysAgo_ScheduleItForToday()
+		{
+			Session.Store(new Post { PublishAt = now.AddDays(-3) });
+			var rescheduler = new PostSchedulingStrategy(Session, now);
+
+			var scheduleDate = now.AddDays(1).AtNoon();
+			var scheduled = rescheduler.Schedule();
+			Assert.Equal(scheduleDate, scheduled);
+		}
+
+		[Fact]
+		public void WhenPostingNewPostWithPublishDateSpecified_AndTheLastPostPublishDateIsAFewDaysAgo_ScheduleItForSpecifiedDate()
+		{
+			Session.Store(new Post { PublishAt = now.AddDays(-3) });
+			var rescheduler = new PostSchedulingStrategy(Session, now);
+
+			var scheduleDate = now.AddHours(1);
+			var scheduled = rescheduler.Schedule(scheduleDate);
+			Assert.Equal(scheduleDate, scheduled);
+			Assert.NotEqual(scheduleDate.AddDays(-2), scheduled);
+		}
+	}
+}
