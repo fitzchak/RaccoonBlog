3c3b5e42090a5828eb4e970b9e72505185aca0bd
Itamar Syn-Hershko <itamar@hibernatingrhinos.com>
2011-09-04 02:47:04 +0300

    Working

--- src/RaccoonBlog.Web/Controllers/WelcomeController.cs
+++ src/RaccoonBlog.Web/Controllers/WelcomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using RaccoonBlog.Web.Infrastructure.Raven;
+using RaccoonBlog.Web.Models;
 
 namespace RaccoonBlog.Web.Controllers
 {
@@ -13,8 +11,50 @@ namespace RaccoonBlog.Web.Controllers
 
         public ActionResult Index()
         {
-            return View();
-        }
+        	AssertConfigurationIsNeeded();
 
+            return View(BlogConfig.New());
+        }
+		
+		[HttpPost]
+		public ActionResult CreateBlog(BlogConfig config)
+		{
+			AssertConfigurationIsNeeded();
+
+			using (var session = DocumentStoreHolder.DocumentStore.OpenSession())
+			{
+				session.Store(config);
+				session.SaveChanges();
+			}
+
+			return RedirectToAction("Success");
+		}
+
+		public ActionResult Success()
+		{
+			BlogConfig config;
+			using (var session = DocumentStoreHolder.DocumentStore.OpenSession())
+			{
+				config = session.Load<BlogConfig>("Blog/Config");
+			}
+
+			return config == null ? View("Index") : View(config);
+		}
+
+		private void AssertConfigurationIsNeeded()
+		{
+			bool canContinue = true;
+			using (var session = DocumentStoreHolder.DocumentStore.OpenSession())
+			{
+				if (session.Load<BlogConfig>("Blog/Config") != null)
+					canContinue = false;
+			}
+
+			if (!canContinue)
+			{
+				Response.Redirect("/");
+				Response.End();
+			}
+		}
     }
 }
