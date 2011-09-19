ac7d57da538084547840efec86cf59a542062514
Ayende Rahien <ayende@ayende.com>
2011-09-19 10:36:46 +0300

    Better error handling if the RavenDB server is not reachable

--- src/RaccoonBlog.Web/Global.asax.cs
+++ src/RaccoonBlog.Web/Global.asax.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
@@ -45,12 +47,44 @@ namespace RaccoonBlog.Web
 			                		ConnectionStringName = "RavenDB"
 			                	}.Initialize();
 
-			IndexCreation.CreateIndexes(typeof (Tags_Count).Assembly, DocumentStore);
+			TryCreatingIndexesOrRedirectToErrorPage();
 
-			RavenProfiler.InitializeFor(MvcApplication.DocumentStore,
+			RavenProfiler.InitializeFor(DocumentStore,
 			                            //Fields to filter out of the output
 			                            "Email", "HashedPassword", "AkismetKey", "GoogleAnalyticsKey", "ShowPostEvenIfPrivate",
 			                            "PasswordSalt", "UserHostAddress");
+		}
+
+		private static void TryCreatingIndexesOrRedirectToErrorPage()
+		{
+			try
+			{
+				IndexCreation.CreateIndexes(typeof (Tags_Count).Assembly, DocumentStore);
+			}
+			catch (WebException e)
+			{
+				var socketException = e.InnerException as SocketException;
+				if(socketException == null)
+					throw;
+
+				switch (socketException.SocketErrorCode)
+				{
+					case SocketError.AddressNotAvailable:
+					case SocketError.NetworkDown:
+					case SocketError.NetworkUnreachable:
+					case SocketError.ConnectionAborted:
+					case SocketError.ConnectionReset:
+					case SocketError.TimedOut:
+					case SocketError.ConnectionRefused:
+					case SocketError.HostDown:
+					case SocketError.HostUnreachable:
+					case SocketError.HostNotFound:
+						HttpContext.Current.Response.Redirect("~/RavenNotReachable.htm");
+						break;
+					default:
+						throw;
+				}
+			}
 		}
 	}
 }
