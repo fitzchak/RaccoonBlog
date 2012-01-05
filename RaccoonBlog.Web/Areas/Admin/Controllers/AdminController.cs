17d03af33ddbe9941e42fb312bcdf83d269bfa31
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-01-05 14:23:39 +0200

    Refactor menu generation out of the controller.

--- RaccoonBlog.Web/Areas/Admin/Controllers/AdminController.cs
+++ RaccoonBlog.Web/Areas/Admin/Controllers/AdminController.cs
@@ -1,6 +1,4 @@
-using System.Collections.Generic;
 using System.Web.Mvc;
-using RaccoonBlog.Web.Areas.Admin.Models;
 using RaccoonBlog.Web.Controllers;
 
 namespace RaccoonBlog.Web.Areas.Admin.Controllers
@@ -8,61 +6,5 @@ namespace RaccoonBlog.Web.Areas.Admin.Controllers
 	[Authorize]
 	public abstract class AdminController : RaccoonController
 	{
-		protected override void OnActionExecuted(ActionExecutedContext filterContext)
-		{
-			base.OnActionExecuted(filterContext);
-			ViewBag.TopMenus = GetTopMenu();
-		}
-
-		private IList<MenuItem> GetTopMenu()
-		{
-			return new List<MenuItem>
-					{
-						new MenuItem
-							{
-								Title = "Back To Blog",
-								SubMenus = null,
-								Url = Url.RouteUrl("homepage"),
-								
-							},
-						new MenuItem
-							{
-								Title = "Posts",
-								SubMenus = new List<MenuItem>
-											{
-												new MenuItem {Title = "All posts", Url = Url.Action("List", "Posts")},
-												new MenuItem{Title = "Create a new post", Url=Url.Action("Add", "Posts")}
-											}
-									  
-							},
-						new MenuItem
-							{
-								Title = "Sections",
-								SubMenus = new List<MenuItem>
-											{
-												new MenuItem {Title = "Sections list", Url = Url.Action("List", "Sections")},
-												new MenuItem {Title = "Add a new section", Url = Url.Action("Add", "Sections")}
-											}
-							},
-						new MenuItem
-							{
-								Title = "Users",
-								Url = Url.Action("Index", "Users"),
-								SubMenus = new List<MenuItem>
-											{
-												new MenuItem {Title = "All users", Url = Url.Action("List", "Users")},
-												new MenuItem {Title = "Add a new user", Url = Url.Action("Add", "Users")}
-											}
-							},
-						new MenuItem
-							{
-								Title = "Tools",
-								SubMenus = new List<MenuItem>
-											{
-												new MenuItem {Title = "Settings", Url = Url.Action("Index", "Settings")},
-											}
-							},
-					};
-		}
 	}
 }
---
+++ RaccoonBlog.Web/Areas/Admin/Helpers/MenuUtil.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using RaccoonBlog.Web.Areas.Admin.Models;
+
+namespace RaccoonBlog.Web.Areas.Admin.Helpers
+{
+	public static class MenuUtil
+	{
+		public static IList<MenuItem> GetTopMenu(UrlHelper url)
+		{
+			return new List<MenuItem>
+			       	{
+			       		new MenuItem
+			       			{
+			       				Title = "Back To Blog",
+			       				SubMenus = null,
+			       				Url = url.RouteUrl("homepage"),
+
+			       			},
+			       		new MenuItem
+			       			{
+			       				Title = "Posts",
+			       				SubMenus = new List<MenuItem>
+			       				           	{
+			       				           		new MenuItem {Title = "All posts", Url = url.Action("List", "Posts")},
+			       				           		new MenuItem {Title = "Create a new post", Url = url.Action("Add", "Posts")}
+			       				           	}
+
+			       			},
+			       		new MenuItem
+			       			{
+			       				Title = "Sections",
+			       				SubMenus = new List<MenuItem>
+			       				           	{
+			       				           		new MenuItem {Title = "Sections list", Url = url.Action("List", "Sections")},
+			       				           		new MenuItem {Title = "Add a new section", Url = url.Action("Add", "Sections")}
+			       				           	}
+			       			},
+			       		new MenuItem
+			       			{
+			       				Title = "Users",
+			       				Url = url.Action("Index", "Users"),
+			       				SubMenus = new List<MenuItem>
+			       				           	{
+			       				           		new MenuItem {Title = "All users", Url = url.Action("List", "Users")},
+			       				           		new MenuItem {Title = "Add a new user", Url = url.Action("Add", "Users")}
+			       				           	}
+			       			},
+			       		new MenuItem
+			       			{
+			       				Title = "Tools",
+			       				SubMenus = new List<MenuItem>
+			       				           	{
+			       				           		new MenuItem {Title = "Settings", Url = url.Action("Index", "Settings")},
+			       				           	}
+			       			},
+			       	};
+		}
+	}
+}
