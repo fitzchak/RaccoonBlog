ddd4f8c61581c2ea6d37eb8ada71dbf28b5077d4
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-01-05 14:49:35 +0200

    Set the IsCurrent value for the menu items

--- RaccoonBlog.Web/Areas/Admin/Helpers/MenuUtil.cs
+++ RaccoonBlog.Web/Areas/Admin/Helpers/MenuUtil.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using RaccoonBlog.Web.Areas.Admin.Models;
 
@@ -8,46 +9,53 @@ namespace RaccoonBlog.Web.Areas.Admin.Helpers
 	{
 		public static IList<MenuItem> GetTopMenu(UrlHelper url)
 		{
-			return new List<MenuItem>
-			       	{
-			       		new MenuItem {Title = "Back To Blog", Url = url.RouteUrl("homepage")},
-			       		new MenuItem
-			       			{
-			       				Title = "Posts",
-			       				SubMenus = new List<MenuItem>
-			       				           	{
-			       				           		new MenuItem {Title = "All posts", Url = url.Action("List", "Posts")},
-			       				           		new MenuItem {Title = "Create a new post", Url = url.Action("Add", "Posts")}
-			       				           	}
-			       			},
-			       		new MenuItem
-			       			{
-			       				Title = "Sections",
-			       				SubMenus = new List<MenuItem>
-			       				           	{
-			       				           		new MenuItem {Title = "Sections list", Url = url.Action("List", "Sections")},
-			       				           		new MenuItem {Title = "Add a new section", Url = url.Action("Add", "Sections")}
-			       				           	}
-			       			},
-			       		new MenuItem
-			       			{
-			       				Title = "Users",
-			       				Url = url.Action("Index", "Users"),
-			       				SubMenus = new List<MenuItem>
-			       				           	{
-			       				           		new MenuItem {Title = "All users", Url = url.Action("List", "Users")},
-			       				           		new MenuItem {Title = "Add a new user", Url = url.Action("Add", "Users")}
-			       				           	}
-			       			},
-			       		new MenuItem
-			       			{
-			       				Title = "Tools",
-			       				SubMenus = new List<MenuItem>
-			       				           	{
-			       				           		new MenuItem {Title = "Settings", Url = url.Action("Index", "Settings")},
-			       				           	}
-			       			},
-			       	};
+			var items = new List<MenuItem>
+			            	{
+			            		new MenuItem {Title = "Back To Blog", Url = url.RouteUrl("homepage")},
+			            		new MenuItem
+			            			{
+			            				Title = "Posts",
+			            				SubMenus = new List<MenuItem>
+			            				           	{
+			            				           		new MenuItem {Title = "All posts", Url = url.Action("Index", "Posts")},
+			            				           		new MenuItem {Title = "Create a new post", Url = url.Action("Add", "Posts")}
+			            				           	}
+			            			},
+			            		new MenuItem
+			            			{
+			            				Title = "Sections",
+			            				SubMenus = new List<MenuItem>
+			            				           	{
+			            				           		new MenuItem {Title = "Sections list", Url = url.Action("Index", "Sections")},
+			            				           		new MenuItem {Title = "Add a new section", Url = url.Action("Add", "Sections")}
+			            				           	}
+			            			},
+			            		new MenuItem
+			            			{
+			            				Title = "Users",
+			            				SubMenus = new List<MenuItem>
+			            				           	{
+			            				           		new MenuItem {Title = "All users", Url = url.Action("Index", "Users")},
+			            				           		new MenuItem {Title = "Add a new user", Url = url.Action("Add", "Users")}
+			            				           	}
+			            			},
+			            		new MenuItem
+			            			{
+			            				Title = "Tools",
+			            				SubMenus = new List<MenuItem>
+			            				           	{
+			            				           		new MenuItem {Title = "Settings", Url = url.Action("Index", "Settings")},
+			            				           	}
+			            			},
+			            	};
+
+			foreach (var menu in items)
+			{
+				var currentUri = url.RequestContext.HttpContext.Request.Url ?? new Uri("/");
+				menu.IsCurrent = currentUri.PathAndQuery == menu.Url;
+			}
+
+			return items;
 		}
 	}
 }
