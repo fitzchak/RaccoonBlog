08851ad6b55fd2d8e5245cf9fb28eef9a37c3226
Fitzchak Yitzchaki <fitzchak@ayende.com>
2012-02-07 09:58:14 +0200

    Partially revert "#45 - Fix and tests for BlogConfig"
    This reverts commit 508c09b482977a98230edba090a223b8872d6889.

--- RaccoonBlog.Web/Controllers/RaccoonController.cs
+++ RaccoonBlog.Web/Controllers/RaccoonController.cs
@@ -8,55 +8,55 @@ using RaccoonBlog.Web.Models;
 
 namespace RaccoonBlog.Web.Controllers
 {
-    public abstract class RaccoonController : RavenController
-    {
-        public const int DefaultPage = 1;
-        public const int PageSize = 25;
+	public abstract class RaccoonController : RavenController
+	{
+		public const int DefaultPage = 1;
+		public const int PageSize = 25;
 
-        private BlogConfig blogConfig;
-        public BlogConfig BlogConfig
-        {
-            get
-            {
-                if (blogConfig == null)
-                {
-                    using (RavenSession.Advanced.DocumentStore.AggressivelyCacheFor(TimeSpan.FromMinutes(5)))
-                    {
-                        blogConfig = RavenSession.Load<BlogConfig>("Blog/Config");
-                    }
+		private BlogConfig blogConfig;
+		public BlogConfig BlogConfig
+		{
+			get
+			{
+				if (blogConfig == null)
+				{
+					using (RavenSession.Advanced.DocumentStore.AggressivelyCacheFor(TimeSpan.FromMinutes(5)))
+					{
+						blogConfig = RavenSession.Load<BlogConfig>("Blog/Config");
+					}
 
-                    if (blogConfig == null && !"welcome".Equals((string)RouteData.Values["controller"], StringComparison.OrdinalIgnoreCase)) // first launch
-                    {
-                        HttpContext.Response.Redirect("~/welcome", true);
-                    }
-                }
-                return blogConfig;
-            }
-        }
+					if (blogConfig == null && "welcome".Equals((string)RouteData.Values["controller"], StringComparison.OrdinalIgnoreCase) == false) // first launch
+					{
+						HttpContext.Response.Redirect("~/welcome", true);
+					}
+				}
+				return blogConfig;
+			}
+		}
 
-        protected override void OnActionExecuted(ActionExecutedContext filterContext)
-        {
-            base.OnActionExecuted(filterContext);
+		protected override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			base.OnActionExecuted(filterContext);
 
-            ViewBag.BlogConfig = BlogConfig;
-        }
+			ViewBag.BlogConfig = BlogConfig;
+		}
 
-        protected int CurrentPage
-        {
-            get
-            {
-                var s = Request.QueryString["page"];
-                int result;
-                if (int.TryParse(s, out result))
-                    return Math.Max(DefaultPage, result);
-                return DefaultPage;
-            }
-        }
+		protected int CurrentPage
+		{
+			get
+			{
+				var s = Request.QueryString["page"];
+				int result;
+				if (int.TryParse(s, out result))
+					return Math.Max(DefaultPage, result);
+				return DefaultPage;
+			}
+		}
 
-        protected new JsonNetResult Json(object data)
-        {
-            var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-            return new JsonNetResult(data, settings);
-        }
-    }
+		protected new JsonNetResult Json(object data)
+		{
+			var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+			return new JsonNetResult(data, settings);
+		}
+	}
 }
