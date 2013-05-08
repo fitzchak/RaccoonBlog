1d9eb191120f47bb7f2c5d35bb6ada0b046a9995
Fitzchak Yitzchaki <fitzchak@ayende.com>
2013-05-08 13:10:56 +0300

    Upgrade to MVC4 and .NET 4.5

--- RaccoonBlog.Web/Properties/AssemblyInfo.cs
+++ RaccoonBlog.Web/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hibernating Rhinos")]
 [assembly: AssemblyProduct("RaccoonBlog.Web")]
-[assembly: AssemblyCopyright("Copyright � Hibernating Rhinos 2011")]
+[assembly: AssemblyCopyright("Copyright © Hibernating Rhinos 2011 - 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
