2e74e3376c8ddadb4bda67d0643f4e1ae96fd6a7
Pawel Pekrol <ppekrol@ais.pl>
2015-02-06 13:05:32 +0100

    added SerieId field to Posts_Series index

--- RaccoonBlog.Web/Infrastructure/Indexes/Posts_Series.cs
+++ RaccoonBlog.Web/Infrastructure/Indexes/Posts_Series.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 
+using RaccoonBlog.Web.Models;
+
+using Raven.Client.Indexes;
+
 namespace RaccoonBlog.Web.Infrastructure.Indexes
 {
-	using RaccoonBlog.Web.Models;
-	using Raven.Client.Indexes;
-
 	public class Posts_Series : AbstractIndexCreationTask<Post, Posts_Series.Result>
 	{
 		public class Result
@@ -15,6 +16,8 @@ namespace RaccoonBlog.Web.Infrastructure.Indexes
 			{
 				Posts = new List<PostInformation>();
 			}
+
+			public int SerieId { get; set; }
 
 			public string Series { get; set; }
 
@@ -47,6 +50,7 @@ namespace RaccoonBlog.Web.Infrastructure.Indexes
 						   select new
 						   {
 							   Series = series,
+							   SerieId = p.Id.Split('/')[1],
 							   Posts = new[] { new { p.Id, p.Title, p.PublishAt } },
 							   Count = 1,
 							   MaxDate = (DateTimeOffset)p.PublishAt,
@@ -55,19 +59,19 @@ namespace RaccoonBlog.Web.Infrastructure.Indexes
 						   };
 
 			Reduce = results => from r in results
-								group r by r.Series
-									into g
-									let maxDate = g.Select(x => x.MaxDate).Max()
-									let minDate = g.Select(x => x.MinDate).Min()
-									select new
-									{
-										Series = g.Key,
-										Posts = g.SelectMany(x => x.Posts),
-										Count = g.Sum((x => x.Count)),
-										MaxDate = maxDate,
-										MinDate = minDate,
-										Days = (int)(maxDate - minDate).TotalDays
-									};
+								group r by r.Series into g
+								let maxDate = g.Select(x => x.MaxDate).Max()
+								let minDate = g.Select(x => x.MinDate).Min()
+								select new
+								{
+									Series = g.Key,
+									SerieId = g.Select(x => x.SerieId).OrderBy(x => x).First(),
+									Posts = g.SelectMany(x => x.Posts),
+									Count = g.Sum(x => x.Count),
+									MaxDate = maxDate,
+									MinDate = minDate,
+									Days = (int)(maxDate - minDate).TotalDays
+								};
 		}
 	}
 }
