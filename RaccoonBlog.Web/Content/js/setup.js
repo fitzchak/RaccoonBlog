f4bd1f0c58133667f5b84d792e540c07080f395c
kamil <kamil@ais.pl>
2015-02-20 09:33:49 +0100

    Merge with Ayende's changes

--- RaccoonBlog.Web/Content/js/setup.js
+++ RaccoonBlog.Web/Content/js/setup.js
@@ -1,5 +1,52 @@
 // JavaScript Document
 
+
+// COOKIES 
+function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+// VARS
+
+var bodyEl = document.body,
+    openbtn = $('#open-button'),
+    showgrid = $('#gridView'),
+    showstack = $('#stackView'),
+    openTags = $('#tags-button'),
+    openArch = $('#archive-button'),
+    openSeries = $('#seriesShow'),
+    isOpen = false,
+    isGrid = true,
+    isTags = false,
+    isArchive = false,
+    isSeries = false;
+
+
+
 (function () {
 
 	$('#archive > ul > li > a').click(function (e) {
@@ -11,19 +58,6 @@
 			$(this).parent().addClass('open');
 		}
 	});
-
-	var bodyEl = document.body,
-		openbtn = $('#open-button'),
-		showgrid = $('#gridView'),
-        showstack = $('#stackView'),
-		openTags = $('#tags-button'),
-		openArch = $('#archive-button'),
-		openSeries = $('#seriesShow'),
-		isOpen = false,
-		isGrid = false,
-		isTags = false,
-		isArchive = false,
-		isSeries = false;
 
 
 	function init() {
@@ -50,15 +84,7 @@
 		isOpen = !isOpen;
 	}
 
-	function toggleGrid() {
-		if (!isGrid) {
-			$(bodyEl).addClass('show-grid');
-			$(showgrid).addClass('active');
-			$(showstack).removeClass('active');
-			isGrid = true;
-			adjustSize();
-		}
-	}
+	
 
 	function toggleStack() {
 		if (isGrid) {
@@ -66,8 +92,20 @@
 			$(showgrid).removeClass('active');
 			$(showstack).addClass('active');
 			isGrid = false;
+		    createCookie('view', 'stack');
 			adjustSize();
 		}
+	}
+    
+	function toggleGrid() {
+	    if (!isGrid) {
+	        $(bodyEl).addClass('show-grid');
+	        $(showgrid).addClass('active');
+	        $(showstack).removeClass('active');
+	        isGrid = true;
+	        createCookie('view', 'grid');
+	        adjustSize();
+	    }
 	}
 
 	function toggleSeries(e) {
@@ -124,6 +162,26 @@ window.onresize = function (event) {
 }
 
 $(document).ready(function (e) {
-	adjustSize();
-});
+    adjustSize();
+    var viewCookieValue = readCookie('view');
+    if (viewCookieValue == 'stack') {
+        $(bodyEl).removeClass('show-grid');
+        $(showgrid).removeClass('active');
+        $(showstack).addClass('active');
+        isGrid = false;
+        createCookie('view', 'stack');
+        adjustSize();
 
+    } else {
+        $(bodyEl).addClass('show-grid');
+        $(showgrid).addClass('active');
+        $(showstack).removeClass('active');
+        isGrid = true;
+        createCookie('view', 'grid');
+        adjustSize();
+
+     
+    }
+        
+        
+});
