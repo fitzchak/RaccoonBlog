5bc539f9ee2d102518c9a59bbac59a5f2cfa4760
Pawel Pekrol <ppekrol@ais.pl>
2015-04-28 15:06:15 +0200

    added oauth keys to configuration

--- RaccoonBlog.Web/App_Start/Startup.Auth.cs
+++ RaccoonBlog.Web/App_Start/Startup.Auth.cs
@@ -1,38 +1,92 @@
-﻿using System;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
+﻿using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
+using Microsoft.Owin.Security.MicrosoftAccount;
 using Microsoft.Owin.Security.Twitter;
+
 using Owin;
+
+using RaccoonBlog.Web.Helpers;
 
 namespace RaccoonBlog.Web
 {
 	public partial class Startup
 	{
-		// For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
 		public void ConfigureAuth(IAppBuilder app)
 		{
-			//app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-			//app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
-
-			 // Enable the application to use a cookie to store information for the signed in user
-			 // and to use a cookie to temporarily store information about a user logging in with a third party login provider
-			 // Configure the sign in cookie
-			 app.UseCookieAuthentication(new CookieAuthenticationOptions
-			 {
+			// Enable the application to use a cookie to store information for the signed in user
+			// and to use a cookie to temporarily store information about a user logging in with a third party login provider
+			// Configure the sign in cookie
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
 				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
 				LoginPath = new PathString("/admin/Login")
 			});
 
 			app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-			//app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
-			//{
-			//	ClientId = "",
-			//	ClientSecret = ""
-			//});
+			AddGoogle(app);
+			AddMicrosoftAccount(app);
+			AddTwitterAccount(app);
+			AddFacebookAccount(app);
+		}
+
+		private static void AddFacebookAccount(IAppBuilder app)
+		{
+			var keys = ConfigurationHelper.FacebookOAuthKeys;
+			if (keys == null)
+				return;
+
+			app.UseFacebookAuthentication(new FacebookAuthenticationOptions
+			{
+				AppId = keys.Item1,
+				AppSecret = keys.Item2
+			});
+		}
+
+		private static void AddTwitterAccount(IAppBuilder app)
+		{
+			var keys = ConfigurationHelper.TwitterOAuthKeys;
+			if (keys == null)
+				return;
+
+			app.UseTwitterAuthentication(new TwitterAuthenticationOptions
+			{
+				ConsumerKey = keys.Item1,
+				ConsumerSecret = keys.Item2
+			});
+		}
+
+		private static void AddGoogle(IAppBuilder app)
+		{
+			var keys = ConfigurationHelper.GoogleOAuthKeys;
+			if (keys == null)
+				return;
+
+			app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+			{
+				ClientId = keys.Item1,
+				ClientSecret = keys.Item2,
+			});
+		}
+
+		private static void AddMicrosoftAccount(IAppBuilder app)
+		{
+			var keys = ConfigurationHelper.MicrosoftOAuthKeys;
+			if (keys == null)
+				return;
+
+			var microsoftAccountAuthenticationOptions = new MicrosoftAccountAuthenticationOptions
+			{
+				ClientId = keys.Item1,
+				ClientSecret = keys.Item2,
+			};
+			microsoftAccountAuthenticationOptions.Scope.Add("wl.basic");
+			microsoftAccountAuthenticationOptions.Scope.Add("wl.emails");
+
+			app.UseMicrosoftAccountAuthentication(microsoftAccountAuthenticationOptions);
 		}
 	}
 }
