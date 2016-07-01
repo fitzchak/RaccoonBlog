52511511cc469cdbdf0677cecd3bef7d72ba1e3c
Grzegorz Lachowski <gregolsky@ravendb.net>
2016-07-01 13:11:37 +0200

    fix: comments sorting by createdAt

--- RaccoonBlog.Web/Controllers/PostDetailsController.cs
+++ RaccoonBlog.Web/Controllers/PostDetailsController.cs
@@ -39,7 +39,9 @@ namespace RaccoonBlog.Web.Controllers
 			var vm = new PostViewModel
 			         {
 			         	Post = post.MapTo<PostViewModel.PostDetails>(),
-			         	Comments = comments.Comments.MapTo<PostViewModel.Comment>(),
+			         	Comments = comments.Comments
+                            .OrderBy(x => x.CreatedAt)
+                            .MapTo<PostViewModel.Comment>(),
 			         	NextPost = RavenSession.GetNextPrevPost(post, true),
 			         	PreviousPost = RavenSession.GetNextPrevPost(post, false),
 			         	AreCommentsClosed = comments.AreCommentsClosed(post, BlogConfig.NumberOfDayToCloseComments),
