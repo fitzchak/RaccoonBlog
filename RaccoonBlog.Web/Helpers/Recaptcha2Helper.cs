b40e09be2872ffa01a9f5a3332e779e640df22dc
Tomasz Opalach <tomtom@ravendb.net>
2018-01-22 13:43:58 +0100

    upgrade captcha to reCAPTCHA V2

---
+++ RaccoonBlog.Web/Helpers/Recaptcha2Helper.cs
@@ -0,0 +1,38 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace RaccoonBlog.Web.Helpers
+{
+    public class Recaptcha2Helper
+    {
+        private static string RecaptchaSecret => ConfigurationManager.AppSettings["Recaptcha/Secret"];
+
+        private static string SiteKey => ConfigurationManager.AppSettings["ReCaptcha/SiteKey"];
+
+        public const string ModelStateErrorKey = "CaptchaNotValid";
+
+        public static async Task<bool> Validate(ModelStateDictionary modelState)
+        {
+            var result = await Recaptcha2Verifier.VerifyResponse(RecaptchaSecret);
+            if (result.IsValid)
+            {
+                return true;
+            }
+
+            modelState.AddModelError(ModelStateErrorKey, result.ErrorMessage);
+            return false;
+        }
+
+        public static MvcHtmlString ScriptRef()
+        {
+            return new MvcHtmlString("<script src='https://www.google.com/recaptcha/api.js'></script>");
+        }
+
+        public static MvcHtmlString Widget()
+        {
+            var result = $"<div class='g-recaptcha' data-sitekey='{SiteKey}'></div>";
+            return new MvcHtmlString(result);
+        }
+    }
+}
---
+++ RaccoonBlog.Web/Helpers/Recaptcha2Verifier.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using NLog;
+
+namespace RaccoonBlog.Web.Helpers
+{
+    public class Recaptcha2Verifier
+    {
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+
+        private const string RecaptchaResponseFieldName = "g-recaptcha-response";
+
+        public static async Task<CaptchaVerificationResult> VerifyResponse(string recaptchaSecret)
+        {
+            var request = HttpContext.Current.Request;
+            if (request == null || !request.Form.HasKeys())
+            {
+                return CaptchaVerificationResult.Error("Captcha response not supplied.");
+            }
+
+            var response = request.Form[RecaptchaResponseFieldName];
+            if (string.IsNullOrEmpty(response))
+            {
+                return CaptchaVerificationResult.Error("Captcha response not supplied.");
+            }
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.google.com"),
+                Timeout = TimeSpan.FromSeconds(30)
+            };
+
+            var formContent = new FormUrlEncodedContent(
+                new[]
+                {
+                    new KeyValuePair<string, string>("secret", recaptchaSecret),
+                    new KeyValuePair<string, string>("response", response)
+                });
+
+            HttpResponseMessage apiResponse = await httpClient.PostAsync(
+                "/recaptcha/api/siteverify", formContent).ConfigureAwait(false);
+
+            string responseContent = await apiResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+            try
+            {
+                JObject responseObj = JObject.Parse(responseContent);
+
+                if (responseObj["success"].Value<bool>())
+                {
+                    return CaptchaVerificationResult.Valid;
+                }
+            }
+            catch (Exception err)
+            {
+                _log.ErrorException("Error validating captcha.", err);
+            }
+
+            return CaptchaVerificationResult.Error("Captcha response is invalid. Please try again.");
+        }
+    }
+
+    public class CaptchaVerificationResult
+    {
+        public bool IsValid { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public static CaptchaVerificationResult Valid => new CaptchaVerificationResult()
+        {
+            IsValid = true
+        };
+
+        public static CaptchaVerificationResult Error(string msg)
+        {
+            return new CaptchaVerificationResult
+            {
+                ErrorMessage = msg
+            };
+        }
+    }
+}
